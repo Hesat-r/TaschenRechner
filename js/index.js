586d9b7d98d020a40794e893239bbf5b1ed06792
Hesat Redzepi <hesatredzepi@Asits-MacBook-Air.local>
2022-06-08 16:09:56 +0200

    bugfixes and other features

--- js/index.js
+++ js/index.js
@@ -31,7 +31,8 @@ buttons.map( button =>{
                     display.innerText += '.';
                     dotalowed = true;
                 }else{
-                    display.innerText += '';
+                    display.innerText += ' ';
+
                 }
                 break;
             case '+':
@@ -90,60 +91,100 @@ buttons.map( button =>{
                 if(numberalowed == true){
                     display.innerText += "1";
                     operatorcheck = false;
+                }else if(numberalowed == false){
+                    display.innerText = '1';
+                    operatorcheck = false;
+                    numberalowed =true; 
                 }
                 break;
             case '2':
                 if(numberalowed == true){
                     display.innerText += "2";
                     operatorcheck = false;
+                }else if(numberalowed == false){
+                    display.innerText = '2';
+                    operatorcheck = false;
+                    numberalowed =true; 
                 }
                 break;
             case '3':
                 if(numberalowed == true){
                     display.innerText += "3";
                     operatorcheck = false;
+                }else if(numberalowed == false){
+                    display.innerText = '3';
+                    operatorcheck = false;
+                    numberalowed =true; 
                 }
                 break;
             case '4':
                 if(numberalowed == true){
                     display.innerText += "4";
                     operatorcheck = false;
+                }else if(numberalowed == false){
+                    display.innerText = '4';
+                    operatorcheck = false;
+                    numberalowed =true; 
                 }
                 break;
             case '5':
                 if(numberalowed == true){
                     display.innerText += "5";
                     operatorcheck = false;
+                }else if(numberalowed == false){
+                    display.innerText = '5';
+                    operatorcheck = false;
+                    numberalowed =true; 
                 }
                 break;
             case '6':
                 if(numberalowed == true){
                     display.innerText += "6";
                     operatorcheck = false;
+                }else if(numberalowed == false){
+                    display.innerText = '6';
+                    operatorcheck = false;
+                    numberalowed =true; 
                 }
                 break;
             case '7':
                 if(numberalowed == true){
                     display.innerText += "7";
                     operatorcheck = false;
+                }else if(numberalowed == false){
+                    display.innerText = '7';
+                    operatorcheck = false;
+                    numberalowed =true; 
                 }
                 break;
             case '8':
                 if(numberalowed == true){
                     display.innerText += "8";
                     operatorcheck = false;
+                }else if(numberalowed == false){
+                    display.innerText = '8';
+                    operatorcheck = false;
+                    numberalowed =true; 
                 }
                 break;
             case '9':
                 if(numberalowed == true){
                     display.innerText += "9";
                     operatorcheck = false;
+                }else if(numberalowed == false){
+                    display.innerText = '9';
+                    operatorcheck = false;
+                    numberalowed =true; 
                 }
                 break;
             case '0':                  
             if(numberalowed == true){
                 display.innerText += "0";
                 operatorcheck = false;
+            }else if(numberalowed == false){
+                display.innerText = '0';
+                operatorcheck = false;
+                numberalowed =true; 
             }
             break;
             default: 
